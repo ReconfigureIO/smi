3083f92800496d7d36264388700425812c13cbc6
zynaptic <chris@zynaptic.com>
2018-02-26 16:55:37 +0000

    Added configurable bus scaling to arbitration tree template.

--- go-template/src/smiMemTemplates/smiArbitrationTreeTemplate.go
+++ go-template/src/smiMemTemplates/smiArbitrationTreeTemplate.go
@@ -33,8 +33,9 @@ type arbitrationTreeConfig struct {
 	SmiMemBusClientConns  []smiMemBusConnectionConfig  // List of client side connections.
 	SmiMemBusServerConn   []smiMemBusConnectionConfig  // Single server side connection.
 	SmiMemBusWireConns    []smiMemBusConnectionConfig  // Internal wire connections.
+	SmiMemBusAssignments  []smiMemBusAssignmentConfig  // List of direct bus assignments.
 	SmiMemBusWidthScalers []smiMemBusWidthScalerConfig // List of bus scaler components.
-	SmiMemBusArbiters     []smiMemBusArbiterConfig     // List of bus scaler components.
+	SmiMemBusArbiters     []smiMemBusArbiterConfig     // List of bus arbiter components.
 }
 
 //
@@ -51,6 +52,7 @@ module {{.ModuleName}} (
   input srst
 );
   {{template "smiMemBusConnectionWireList" .SmiMemBusWireConns}}
+  {{range .SmiMemBusAssignments}}{{template "smiMemBusAssignment" .}}{{end}}
   {{range .SmiMemBusWidthScalers}}{{template "smiMemBusWidthScaler" .}}{{end}}
   {{range .SmiMemBusArbiters}}{{template "smiMemBusArbiter" .}}{{end}}
 endmodule
@@ -71,6 +73,7 @@ func getArbitrationTreeTemplate() *template.Template {
 		templGroup = template.Must(templGroup.Parse(smiMemBusConnectionClientPortTemplate))
 		templGroup = template.Must(templGroup.Parse(smiMemBusConnectionServerPortTemplate))
 		templGroup = template.Must(templGroup.Parse(smiMemBusConnectionWireListTemplate))
+		templGroup = template.Must(templGroup.Parse(smiMemBusAssignmentTemplate))
 		templGroup = template.Must(templGroup.Parse(smiMemBusArbiterTemplate))
 		templGroup = template.Must(templGroup.Parse(smiMemBusWidthScalerTemplate))
 		templGroup = template.Must(templGroup.Parse(smiMemBusArbitrationTreeTemplate))
@@ -82,7 +85,9 @@ func getArbitrationTreeTemplate() *template.Template {
 //
 // Generates an arbitration tree configuration given the supplied parameters.
 //
-func configureArbitrationTree(moduleName string, numClients uint) (arbitrationTreeConfig, error) {
+func configureArbitrationTree(moduleName string, numClients uint,
+	scalingFactor uint) (arbitrationTreeConfig, error) {
+
 	var arbitrationTree = arbitrationTreeConfig{}
 	arbitrationTree.ModuleName = moduleName
 
@@ -94,38 +99,51 @@ func configureArbitrationTree(moduleName string, numClients uint) (arbitrationTr
 	} else if numClients == 1 {
 		// For a single client, just implement scaling from the inputs to outputs.
 		clientConn := smiMemBusConnectionConfig{"smiMemClientReq0", "smiMemClientResp0", 8}
-		serverConn := smiMemBusConnectionConfig{"smiMemServerReq", "smiMemServerResp", 64}
+		serverConn := smiMemBusConnectionConfig{"smiMemServerReq", "smiMemServerResp", scalingFactor * 8}
 		arbitrationTree.SmiMemBusClientConns = []smiMemBusConnectionConfig{clientConn}
 		arbitrationTree.SmiMemBusServerConn = []smiMemBusConnectionConfig{serverConn}
-		arbitrationTree.SmiMemBusWidthScalers = []smiMemBusWidthScalerConfig{
-			{"busWidthScaler", 8, 8, clientConn, serverConn}}
+		if scalingFactor > 1 {
+			arbitrationTree.SmiMemBusWidthScalers = []smiMemBusWidthScalerConfig{
+				{"busWidthScaler", scalingFactor, 8, clientConn, serverConn}}
+		} else {
+			arbitrationTree.SmiMemBusAssignments = []smiMemBusAssignmentConfig{
+				{clientConn, serverConn}}
+		}
 
 	} else if numClients <= 3 {
-		// For two to three clients, implement single scaling arbiter with
-		// additional scaling on the client side. Start by building the
-		// client side connections and bus width scalers.
+		// For two to three clients, implement single arbiter with scaling on the
+		// client side. Start by building the client side connections and bus width scalers.
 		arbitrationTree.SmiMemBusClientConns = make([]smiMemBusConnectionConfig, numClients)
 		arbitrationTree.SmiMemBusWireConns = make([]smiMemBusConnectionConfig, numClients)
-		arbitrationTree.SmiMemBusWidthScalers = make([]smiMemBusWidthScalerConfig, numClients)
+		if scalingFactor > 1 {
+			arbitrationTree.SmiMemBusWidthScalers = make([]smiMemBusWidthScalerConfig, numClients)
+		} else {
+			arbitrationTree.SmiMemBusAssignments = make([]smiMemBusAssignmentConfig, numClients)
+		}
 		for i := uint(0); i < numClients; i++ {
 			clientConn := smiMemBusConnectionConfig{
 				fmt.Sprintf("smiMemClientReq%d", i),
 				fmt.Sprintf("smiMemClientResp%d", i), 8}
 			wireConn := smiMemBusConnectionConfig{
 				fmt.Sprintf("smiMemScaledReq%d", i),
-				fmt.Sprintf("smiMemScaledResp%d", i), 32}
+				fmt.Sprintf("smiMemScaledResp%d", i), scalingFactor * 8}
 			arbitrationTree.SmiMemBusClientConns[i] = clientConn
 			arbitrationTree.SmiMemBusWireConns[i] = wireConn
-			arbitrationTree.SmiMemBusWidthScalers[i] = smiMemBusWidthScalerConfig{
-				fmt.Sprintf("busWidthScaler%d", i), 4, 8,
-				clientConn, wireConn}
+			if scalingFactor > 1 {
+				arbitrationTree.SmiMemBusWidthScalers[i] = smiMemBusWidthScalerConfig{
+					fmt.Sprintf("busWidthScaler%d", i), scalingFactor, 8, clientConn, wireConn}
+			} else {
+				arbitrationTree.SmiMemBusAssignments[i] = smiMemBusAssignmentConfig{
+					clientConn, wireConn}
+			}
 		}
 
 		// Add the server side connection and arbiter component.
 		serverConn := smiMemBusConnectionConfig{
-			"smiMemServerReq", "smiMemServerResp", 64}
+			"smiMemServerReq", "smiMemServerResp", scalingFactor * 8}
 		arbitrationTree.SmiMemBusArbiters = []smiMemBusArbiterConfig{
-			{"busArbiter", 32, 4, 32, 4, true, arbitrationTree.SmiMemBusWireConns, serverConn}}
+			{"busArbiter", 32, 4, scalingFactor * 8, 4, false,
+				arbitrationTree.SmiMemBusWireConns, serverConn}}
 		arbitrationTree.SmiMemBusServerConn = []smiMemBusConnectionConfig{serverConn}
 
 	} else if numClients <= 64 {
@@ -157,20 +175,39 @@ func configureArbitrationTree(moduleName string, numClients uint) (arbitrationTr
 		}
 		fmt.Printf(" ]\n")
 
+		// Determine bus scaling parameters.
+		flitWidthLayer0 := uint(32)
+		doScalingLayer0 := true
+		if scalingFactor < 8 {
+			flitWidthLayer0 = scalingFactor * 8
+			doScalingLayer0 = false
+		}
+		flitWidthLayer1 := uint(16)
+		doScalingLayer1 := true
+		if scalingFactor < 4 {
+			flitWidthLayer1 = scalingFactor * 8
+			doScalingLayer1 = false
+		}
+		doScalingLayer2 := true
+		if scalingFactor < 2 {
+			doScalingLayer2 = false
+		}
+
 		// Add the main arbitration component.
 		serverConn := smiMemBusConnectionConfig{
-			"smiMemServerReq", "smiMemServerResp", 64}
+			"smiMemServerReq", "smiMemServerResp", scalingFactor * 8}
 		busWireConns := make([]smiMemBusConnectionConfig, fanInLayer0)
 		arbitrationTree.SmiMemBusWireConns = make([]smiMemBusConnectionConfig, fanInLayer0)
 		for i := uint(0); i < fanInLayer0; i++ {
-			busWireConn := smiMemBusConnectionConfig{
-				fmt.Sprintf("smiWireReqL0I%d", i), fmt.Sprintf("smiWireRespL0I%d", i), 32}
+			busWireConn := smiMemBusConnectionConfig{fmt.Sprintf("smiWireReqL0I%d", i),
+				fmt.Sprintf("smiWireRespL0I%d", i), flitWidthLayer0}
 			busWireConns[i] = busWireConn
 			arbitrationTree.SmiMemBusWireConns[i] = busWireConn
 		}
 		arbitrationTree.SmiMemBusArbiters = make([]smiMemBusArbiterConfig, 1)
 		arbitrationTree.SmiMemBusArbiters[0] = smiMemBusArbiterConfig{
-			"busArbiterL0I0", 32, 4, 32, 4, true, busWireConns, serverConn}
+			"busArbiterL0I0", 32, 4, flitWidthLayer0, 4, doScalingLayer0,
+			busWireConns, serverConn}
 		arbitrationTree.SmiMemBusServerConn = []smiMemBusConnectionConfig{serverConn}
 
 		// Add the second layer of arbiters.
@@ -179,14 +216,14 @@ func configureArbitrationTree(moduleName string, numClients uint) (arbitrationTr
 			for j := uint(0); j < fanInLayer1; j++ {
 				busWireConns[j] = smiMemBusConnectionConfig{
 					fmt.Sprintf("smiWireReqL1I%d", i*fanInLayer1+j),
-					fmt.Sprintf("smiWireRespL1I%d", i*fanInLayer1+j), 16}
+					fmt.Sprintf("smiWireRespL1I%d", i*fanInLayer1+j), flitWidthLayer1}
 			}
 			serverSideConn := smiMemBusConnectionConfig{
 				fmt.Sprintf("smiWireReqL0I%d", i),
-				fmt.Sprintf("smiWireRespL0I%d", i), 32}
+				fmt.Sprintf("smiWireRespL0I%d", i), flitWidthLayer0}
 			busArbiter := smiMemBusArbiterConfig{
-				fmt.Sprintf("busArbiterL1I%d", i), 32, 4, 16, 4,
-				true, busWireConns, serverSideConn}
+				fmt.Sprintf("busArbiterL1I%d", i), 32, 4, flitWidthLayer1, 4,
+				doScalingLayer1, busWireConns, serverSideConn}
 			arbitrationTree.SmiMemBusArbiters = append(
 				arbitrationTree.SmiMemBusArbiters, busArbiter)
 			arbitrationTree.SmiMemBusWireConns = append(
@@ -197,19 +234,27 @@ func configureArbitrationTree(moduleName string, numClients uint) (arbitrationTr
 		clientIndex := uint(0)
 		arbitrationTree.SmiMemBusClientConns = make([]smiMemBusConnectionConfig, 0)
 		arbitrationTree.SmiMemBusWidthScalers = make([]smiMemBusWidthScalerConfig, 0)
+		arbitrationTree.SmiMemBusAssignments = make([]smiMemBusAssignmentConfig, 0)
 		for i := uint(0); i < numServers; i++ {
 			serverSideConn := smiMemBusConnectionConfig{
 				fmt.Sprintf("smiWireReqL1I%d", i),
-				fmt.Sprintf("smiWireRespL1I%d", i), 16}
+				fmt.Sprintf("smiWireRespL1I%d", i), flitWidthLayer1}
 			if fanInsLayer2[i] == 1 {
 				clientSideConn := smiMemBusConnectionConfig{
 					fmt.Sprintf("smiMemClientReq%d", clientIndex),
 					fmt.Sprintf("smiMemClientResp%d", clientIndex), 8}
-				busWidthScaler := smiMemBusWidthScalerConfig{
-					fmt.Sprintf("busWidthScalerL2I%d", i), 2, 8,
-					clientSideConn, serverSideConn}
-				arbitrationTree.SmiMemBusWidthScalers = append(
-					arbitrationTree.SmiMemBusWidthScalers, busWidthScaler)
+				if doScalingLayer2 {
+					busWidthScaler := smiMemBusWidthScalerConfig{
+						fmt.Sprintf("busWidthScalerL2I%d", i), 2, 8,
+						clientSideConn, serverSideConn}
+					arbitrationTree.SmiMemBusWidthScalers = append(
+						arbitrationTree.SmiMemBusWidthScalers, busWidthScaler)
+				} else {
+					busAssignment := smiMemBusAssignmentConfig{
+						clientSideConn, serverSideConn}
+					arbitrationTree.SmiMemBusAssignments = append(
+						arbitrationTree.SmiMemBusAssignments, busAssignment)
+				}
 				arbitrationTree.SmiMemBusClientConns = append(
 					arbitrationTree.SmiMemBusClientConns, clientSideConn)
 			} else {
@@ -221,7 +266,7 @@ func configureArbitrationTree(moduleName string, numClients uint) (arbitrationTr
 				}
 				busArbiter := smiMemBusArbiterConfig{
 					fmt.Sprintf("busArbiterL2I%d", i), 32, 4, 8, 4,
-					true, clientSideConns, serverSideConn}
+					doScalingLayer2, clientSideConns, serverSideConn}
 				arbitrationTree.SmiMemBusArbiters = append(
 					arbitrationTree.SmiMemBusArbiters, busArbiter)
 				arbitrationTree.SmiMemBusClientConns = append(
